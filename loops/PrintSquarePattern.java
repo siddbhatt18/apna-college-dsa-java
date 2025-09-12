d52411db652e9c46be500ecf6611e38b5f16b744
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-09-12 21:29:43 +0530

    Delete loops directory

--- loops/PrintSquarePattern.java
+++
@@ -1,7 +0,0 @@
-public class PrintSquarePattern {
-    public static void main(String[] args) {
-        for(int i = 1; i <= 4; i++) {
-            System.out.println("* * * * ");
-        }
-    }
-}
