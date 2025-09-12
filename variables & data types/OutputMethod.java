04024b1a87f1d6f6235aa12092ba64afabc0fde4
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-09-12 21:30:08 +0530

    Delete variables & data types directory

--- variables & data types/OutputMethod.java
+++
@@ -1,8 +0,0 @@
-public class OutputMethod {
-    public static void main(String[] args) {
-        System.out.println("line 1");
-        System.out.print("line 2 ");
-        System.out.print("line 3 \n");
-        System.out.print("line 4");
-    }
-}
--- variables & data types/TypeConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class TypeConversion {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float num = sc.nextInt();
-        System.out.println(num);
-        sc.close();
-    }
-}
